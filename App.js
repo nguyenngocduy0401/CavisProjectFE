969a0ae1379860cac15faea245c694285ce19284
nguyenngocduy0401 <132876181+nguyenngocduy0401@users.noreply.github.com>
2024-05-25 12:42:08 +0700

    Reapply "Add Login UI"
    This reverts commit 13a736aaca067815d6db7c8930f9c4f6af4544dd.

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Login from './src/screens/login/Login'
+export default function App() {
+  return (
+    <View>
+      <Login/>
+    </View>
+  )
+}
